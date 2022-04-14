0407618cb14b8b017ccc19bea95ab7d3c0559788
Yoon Jae Ho <dkssud8159@naver.com>
2022-04-15 04:36:50 +0900

    차선을 탐색하고 그리는 행위의 순서가 이상해서 뒤집음, make sliding window number 12, 한쪽 차선 찾지 못할 때 처리 , 미드라인 그리는 것 추가

--- src/sliding_drive.cpp
+++ src/sliding_drive.cpp
@@ -5,44 +5,65 @@ using namespace std;
 using namespace cv;
 
 Vec4f n_window_sliding(int left_start, int right_start, Mat roi, Mat v_thres, int w = 320, int h = 240, int nwindows = 8, int window_width = 40, int window_height = 30, int margin = 15) {
-	vector<int> lx, ly, rx, ry;
-	vector<Point> lpoints(nwindows), rpoints(nwindows);
+	vector<Point> lpoints(nwindows), rpoints(nwindows), mpoints(nwindows);
 
-	int ll = 0, lr = 0; int rl = 640, rr = 640;
-	// window search
-	for (int window = 0; window < nwindows; window++) {
-		// window y up value / low value
-		int win_y_high = h - (window + 1) * window_height;
-		int win_y_low = h - window * window_height;
+	// init value setting
+	int lane_mid = w / 2;
+
+	int win_y_high = h - window_height;
+	int win_y_low = h;
+
+	int win_x_leftb_right = left_start + margin;
+	int win_x_leftb_left = left_start - margin;
+
+	int win_x_rightb_right = right_start + margin;
+	int win_x_rightb_left = right_start - margin;
+
+	lpoints[0] = Point(left_start, (int)((win_y_high + win_y_low) / 2));
+	rpoints[0] = Point(right_start, (int)((win_y_high + win_y_low) / 2));
+	mpoints[0] = Point((int)((left_start + right_start) / 2), (int)((win_y_high + win_y_low) / 2));
+
+	// init box draw
+	rectangle(roi, Rect(win_x_leftb_left, win_y_high, window_width, window_height), Scalar(0, 0, 255), 2);
+	rectangle(roi, Rect(win_x_rightb_left, win_y_high, window_width, window_height), Scalar(0, 0, 255), 2);
 
 
-		int win_x_leftb_right = left_start + margin;
-		int win_x_leftb_left = left_start - margin;
 
-		int win_x_rightb_right = right_start + margin;
-		int win_x_rightb_left = right_start - margin;
+	// window search start, i drew the init box at the bottom, so i start from 1 to nwindows
+	for (int window = 1; window < nwindows; window++) {
 
-		// draw window at v_thres
-		rectangle(roi, Rect(win_x_leftb_left, win_y_high, window_width, window_height), (255, 255, 255), 2);
-		rectangle(roi, Rect(win_x_rightb_left, win_y_high, window_width, window_height), (255, 255, 255), 2);
+		win_y_high = h - (window + 1) * window_height;
+		win_y_low = h - window * window_height;
 
-		// box mid point
-		int high = win_y_high + 4;
+		win_x_leftb_right = left_start + margin;
+		win_x_leftb_left = left_start - margin;
 
-		int pixel_thres = window_width * 0.4;
-		int li = 0;
-		// window의 위치를 고려해서 벡터에 집어넣으면 불필요한 부분이 많아질 수 있다. 어차피 0의 개수를 구하기 위한 벡터이므로 0부터 window_width 개수만큼 생성
-		vector<int> lhigh_vector(window_width + 1);
+		win_x_rightb_right = right_start + margin;
+		win_x_rightb_left = right_start - margin;
+
+
+		// i will detect the lane at top of box
+//		int offset = win_y_high + 4;
+		// i will detect the lane at bottom of box
+//		int offset = win_y_low - 4;
+		// i will detect the lane at the mid of box 
+		int offset = (int)((win_y_high + win_y_low) / 2);
+
+		int pixel_thres = window_width * 0.2;
+		int ll = 0, lr = 0; int rl = 960, rr = 960;
+		int li = 0; // nonzero가 몇개인지 파악하기 위한 벡터에 사용될 인자
+		// window의 위치를 고려해서 벡터에 집어넣으면 불필요한 부분이 많아질 수 있다. 어차피 0의 개수를 구하기 위한 벡터이므로 0부터 window_width+1 개수만큼 생성
+		vector<int> lhigh_vector(window_width + 1); // nonzero가 몇개 인지 파악할 때 사용할 벡터
 		for (auto x = win_x_leftb_left; x < win_x_leftb_right; x++) {
 			li++;
-			lhigh_vector[li] = v_thres.at<uchar>(high, x);
+			lhigh_vector[li] = v_thres.at<uchar>(offset, x);
 
 			// 차선의 중앙을 계산하기 위해 255 시작점과 255 끝점을 계산
-			if (v_thres.at<uchar>(high, x) == 255 && ll == 0) {
+			if (v_thres.at<uchar>(offset, x) == 255 && ll == 0) {
 				ll = x;
 				lr = x;
 			}
-			if (v_thres.at<uchar>(high, x) == 255 && lr != 0) {
+			if (v_thres.at<uchar>(offset, x) == 255 && lr != 0) {
 				lr = x;
 			}
 		}
@@ -50,7 +71,7 @@ Vec4f n_window_sliding(int left_start, int right_start, Mat roi, Mat v_thres, in
 		// window안에서 0이 아닌 픽셀의 개수를 구함
 		int lnonzero = countNonZero(lhigh_vector);
 		// 255인 픽셀의 개수가 threshold를 넘으면, 방금 구했던 255 픽셀 시작 지점과 끝 지점의 중앙 값을 다음 window의 중앙으로 잡는다.
-		if (lnonzero > pixel_thres) {
+		if (lnonzero >= pixel_thres) {
 			left_start = (ll + lr) / 2;
 		}
 
@@ -58,52 +79,83 @@ Vec4f n_window_sliding(int left_start, int right_start, Mat roi, Mat v_thres, in
 		vector<int> rhigh_vector(window_width + 1);
 		for (auto x = win_x_rightb_left; x < win_x_rightb_right; x++) {
 			ri++;
-			rhigh_vector[ri] = v_thres.at<uchar>(high, x);
-			if (v_thres.at<uchar>(high, x) == 255 && rl == 640) {
+			rhigh_vector[ri] = v_thres.at<uchar>(offset, x);
+			if (v_thres.at<uchar>(offset, x) == 255 && rl == 960) {
 				rl = x;
 				rr = x;
 			}
-			if (v_thres.at<uchar>(high, x) == 255 && lr != 640) {
+			if (v_thres.at<uchar>(offset, x) == 255 && lr != 960) {
 				rr = x;
 			}
 		}
 
 		int rnonzero = countNonZero(rhigh_vector);
-		if (rnonzero > pixel_thres) {
+		if (rnonzero >= pixel_thres) {
 			right_start = (rl + rr) / 2;
 		}
 
-		// 직선의 방정식을 구하기 위한 vector, 현재는 특정 offset에 대한 차선 인식을 수행할 것이므로 사용 x
+		// 차선 중앙과 탐지한 차선과의 거리 측정
+		int lane_mid = (right_start + left_start) / 2;
+		int left_diff = lane_mid - left_start;
+		int right_diff = -(lane_mid - right_start);
 
+
+		// 한쪽 차선의 nonzero가 임계값을 넘지 못할 경우 중간을 기점으로 반대편 차선 위치를 기준으로 대칭
+		if (lnonzero < pixel_thres && rnonzero > pixel_thres) {
+			left_start = lane_mid - right_diff;
+			lane_mid = right_start - right_diff;
+		}
+		else if (lnonzero > pixel_thres && rnonzero < pixel_thres) {
+			right_start = lane_mid + left_diff;
+			lane_mid = left_start + left_diff;
+		}
+
+
+
+		// draw window at v_thres
+		rectangle(roi, Rect(win_x_leftb_left, win_y_high, window_width, window_height), (255, 255, 255), 2);
+		rectangle(roi, Rect(win_x_rightb_left, win_y_high, window_width, window_height), (255, 255, 255), 2);
+
+
+
+		mpoints[window] = Point(lane_mid, (int)((win_y_high + win_y_low) / 2));
 		lpoints[window] = Point(left_start, (int)((win_y_high + win_y_low) / 2));
 		rpoints[window] = Point(right_start, (int)((win_y_high + win_y_low) / 2));
 	}
 
-	Vec4f left_line, right_line;
+	Vec4f left_line, right_line, mid_line;
 	fitLine(lpoints, left_line, DIST_L2, 0, 0.01, 0.01); // 출력의 0,1 번째 인자는 단위벡터, 3,4번째 인자는 선 위의 한 점
 	fitLine(rpoints, right_line, DIST_L2, 0, 0.01, 0.01);
+	fitLine(mpoints, mid_line, DIST_L2, 0, 0.01, 0.01);
 
+	// 방향이 항상 아래를 향하도록 만들기 위해 단위 벡터의 방향을 바꿔준다.
 	if (left_line[1] > 0) {
 		left_line[1] = -left_line[1];
 	}
-
-	int lx0 = left_line[2], ly0 = left_line[3]; // 선 위의 한 점
-	int lx1 = lx0 - 300 * left_line[0], ly1 = ly0 - 300 * left_line[1]; // 단위 벡터 -> 그리고자 하는 길이를 빼주거나 더해줌
-
 	if (right_line[1] > 0) {
 		right_line[1] = -right_line[1];
 	}
+	if (mid_line[1] > 0) {
+		mid_line[1] = -mid_line[1];
+	}
 
-	int rx0 = right_line[2], ry0 = right_line[3];
-	int rx1 = rx0 - 300 * right_line[0], ry1 = ry0 - 300 * right_line[1];
+	int lx0 = left_line[2], ly0 = left_line[3] + h / 2; // 선 위의 한 점
+	int lx1 = lx0 + h * left_line[0], ly1 = ly0 + h * left_line[1]; // 단위 벡터 -> 그리고자 하는 길이를 빼주거나 더해줌
 
-	line(roi, Point(lx0, ly0), Point(lx1, ly1), Scalar(255, 0, 0), 2);
-	line(roi, Point(rx0, ry0), Point(rx1, ry1), Scalar(255, 0, 0), 2);
+	int rx0 = right_line[2], ry0 = right_line[3] + h / 2;
+	int rx1 = rx0 + h * right_line[0], ry1 = ry0 + h * right_line[1];
 
-	return left_line, right_line;
+	int mx0 = mid_line[2], my0 = mid_line[3] + h / 2;
+	int mx1 = mx0 + h * mid_line[0], my1 = my0 + h * mid_line[1];
+
+	line(roi, Point(lx0, ly0), Point(lx1, ly1), Scalar(0, 0, 255), 3);
+	line(roi, Point(rx0, ry0), Point(rx1, ry1), Scalar(0, 0, 255), 3);
+	line(roi, Point(mx0, my0), Point(mx1, my1), Scalar(0, 0, 255), 3);
+
+	return left_line, right_line, mid_line;
 }
 
-void draw_line(Mat frame, Mat roi, Vec4f left_line, Vec4f right_line, Mat per_mat_tosrc, int width = 640, int height = 480) {
+void draw_line(Mat frame, Mat roi, Vec4f left_line, Vec4f right_line, Mat per_mat_tosrc, int width = 960, int height = 480) {
 	Mat newframe;
 	warpPerspective(roi, newframe, per_mat_tosrc, Size(width, height), INTER_LINEAR);
 
@@ -124,7 +176,7 @@ int main()
 	int height = cvRound(cap.get(CAP_PROP_FRAME_HEIGHT));
 
 	// warped image size
-	int w = (int)width * 1.5, h = (int)height * 1;
+	int w = (int)width * 1.5, h = (int)height * 1.5;
 
 	// point about warp transform
 	vector<Point2f> src_pts(4);
@@ -134,12 +186,12 @@ int main()
 //	src_pts[0] = Point2f(0, 420); src_pts[1] = Point2f(213, 280); src_pts[2] = Point2f(395, 280); src_pts[3] = Point2f(595, 420);
 
 	// 파란색 선 없는 roi
-	src_pts[0] = Point2f(10, 395); src_pts[1] = Point2f(200, 280); src_pts[2] = Point2f(400, 280); src_pts[3] = Point2f(570, 395);
+	src_pts[0] = Point2f(0, 395); src_pts[1] = Point2f(198, 280); src_pts[2] = Point2f(403, 280); src_pts[3] = Point2f(580, 395);
 	dst_pts[0] = Point2f(0, h - 1); dst_pts[1] = Point2f(0, 0); dst_pts[2] = Point2f(w - 1, 0); dst_pts[3] = Point2f(w - 1, h - 1);
 
 	// point about polylines
 	vector<Point> pts(4);
-	pts[0] = Point(10, 395); pts[1] = Point(200, 280); pts[2] = Point(400, 280); pts[3] = Point(570, 395);
+	pts[0] = Point(src_pts[0]); pts[1] = Point(src_pts[1]); pts[2] = Point(src_pts[2]); pts[3] = Point(src_pts[3]);
 
 	//	pts[0] = Point(0, 420); pts[1] = Point(213, 280); pts[2] = Point(395, 280); pts[3] = Point(595, 420);
 
@@ -189,8 +241,9 @@ int main()
 
 
 #elif 1	// 2-1 hsv -> gaussian -> inRange -> canny
-		Mat hsv, v_thres;
-		int lane_binary_thres = 130; // contrast : 185
+		Mat hsv;
+		Mat v_thres = Mat::zeros(w, h, CV_8UC1);
+		int lane_binary_thres = 125; // contrast : 155
 		cvtColor(roi, hsv, COLOR_BGR2HSV);
 
 		// split H/S/V
@@ -234,15 +287,16 @@ int main()
 #endif	
 
 		// define constant for sliding window
-		int nwindows = 15;
+		int nwindows = 12;
 		int window_height = (int)(h / nwindows);
-		int window_width = (int)(window_height * 3);
+		int window_width = (int)(w / nwindows * 1.5);
 
 		int margin = window_width / 2;
 
 		// 첫위치 지정
+		int cnt = 0;
 		int left_l_init = 0, left_r_init = 0;
-		int right_l_init = 640, right_r_init = 640;
+		int right_l_init = 960, right_r_init = 960;
 		for (auto x = 0; x < w; x++) {
 			if (x < w / 2) {
 				if (v_thres.at<uchar>(h - 1, x) == 255 && left_l_init == 0) {
@@ -254,11 +308,11 @@ int main()
 				}
 			}
 			else {
-				if (v_thres.at<uchar>(h - 1, x) == 255 && right_l_init == 640) {
+				if (v_thres.at<uchar>(h - 1, x) == 255 && right_l_init == 960) {
 					right_l_init = x;
 					right_r_init = x;
 				}
-				if (v_thres.at<uchar>(h - 1, x) == 255 && right_r_init != 640) {
+				if (v_thres.at<uchar>(h - 1, x) == 255 && right_r_init != 960) {
 					right_r_init = x;
 				}
 			}
